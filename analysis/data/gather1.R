7eb8f6d8236c4e987a880148e72b81db88d84df1
kevin <kthompson395@hotmail.com>
2019-06-15 00:31:03 -0500

    finished gather1.R file for cleaning and gathering Beers.csv

---
+++ analysis/data/gather1.R
@@ -0,0 +1,15 @@
+#############
+# Gather file for Beers.csv
+# Authors: Kevin Thompson and Stuart Miller
+# Last updated June 15, 2019
+#############
+
+beer_data <- read.csv("analysis/data/Beers.csv")
+
+
+# Names and IDs should be read in as characters, not factors
+beer_data$Name <- as.character(beer_data$Name)
+beer_data$Beer_ID <- as.character(beer_data$Beer_ID)
+beer_data$Brewery_id <- as.character(beer_data$Brewery_id)
+beer_data$Ounce_groups <- as.factor(beer_data$Ounces)
+beer_data <- subset(beer_data, select = -c(Ounces))
