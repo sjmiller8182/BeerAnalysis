6b2bdca6cf669a3931737374532c2ac0ec8ba923
kevin <kthompson395@hotmail.com>
2019-06-26 23:30:16 -0500

    removed abbreviations function and added abbreviation data

---
+++ analysis/data/gather3.R
@@ -0,0 +1,9 @@
+###########
+# Gather file for for Abbreviations.csv
+# Authors: Kevin Thompson and Stuart Miller
+# Last updated June 26, 2019
+###########
+
+library(tidyverse)
+
+abbreviation_data <- read_csv("Abbreviations.csv")
